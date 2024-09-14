039691d5fa5203f40321565a0ccf10e40eab5ec9
unknown <kachwalahasan14@gmail.com>
2024-09-14 11:36:48 +0530

    change

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import Home from './components/home';
 import About from './components/aboutUs';
-import Product from './components/prouduct';
+import Product from './components/product';
 
 function App() {
   return (
