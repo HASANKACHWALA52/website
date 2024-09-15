868205b6196ad9e85cf2ac6eab751d253c4f0b94
unknown <kachwalahasan14@gmail.com>
2024-09-15 11:09:10 +0530

    change

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
-import Home from './components/home';
-import About from './components/aboutUs';
-import Product from './components/product';
+import Home from '.../components/home';
+import About from '.../components/aboutUs';
+import Product from '.../components/product';
 
 function App() {
   return (
